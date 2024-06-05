4498205ab71a7c2f5071e3556b68725beabc3bb7
Pilar Sierra <pilarsierra126@gmail.com>
2024-06-04 21:04:18 -0300

    primer commit

---
+++ js/tarea1.js
@@ -0,0 +1,19 @@
+//Tarea
+// Crear dos variables de tipo number
+// mostrar por consola los resultados de las siguientes operaciones:
+// Sumar las dos variables
+// Restar las dos variables
+// Multiplicar las dos variables
+// Dividir ambas variables
+// comparar si la primera variable es mayor o igual que la segunda
+// Comparar si la primera variable es distinta de la segunda
+
+let numero1 = 23
+let numero2 = 15
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 >= numero2)
+console.log(numero1 != numero2)
